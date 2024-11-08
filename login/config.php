8085c266aa1dbda01adb05941b5b3159fc5059fc
Rekt Developer <isaac.likhon@yahoo.com>
2024-11-09 00:40:00 +0600

    Update config.php

--- login/config.php
+++ login/config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = '₹';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'spectacles';
+$db_username = 'jlhcinnsjn_scripters';
+$db_password = 'jlhcinnsjn_scripters';
+$db_name = 'jlhcinnsjn_scripters';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
